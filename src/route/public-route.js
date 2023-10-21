bf22a2f762ca0634385f50effd13bbbde2f2942c
ahmadchoirulhuda <huda60399@gmail.com>
2023-10-21 20:46:01 +0700

    route public api-docs

--- src/route/public-route.js
+++ src/route/public-route.js
@@ -1,6 +1,9 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
+import swagger from "swagger-ui-express";
+import apiDocs from "../api-docs-management/restfull-api.json" assert { type: "json" };
 
 export const publicRoute = express.Router();
+publicRoute.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 publicRoute.post("/users/registration", userController.registrasi);
 publicRoute.post("/users/login", userController.login);
